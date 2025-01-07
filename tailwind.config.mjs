9c60798d8b044f925fc090c1c75c3997be85acc9
@webdeveloper130390 <abdullokhpakhlavonov@gmail.com>
2025-01-07 11:59:55 +0500

    floxsy-turnoff-dark

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,4 @@
 export default {
-  darkMode: false, 
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,8 +7,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "#ffffff",
+        foreground: "#000000", 
       },
     },
   },
