4fd947c9a9fe7b715fe8264e6ddb96bfd71d9b2d
@webdeveloper130390 <abdullokhpakhlavonov@gmail.com>
2025-01-08 15:41:04 +0500

    floxsy-otp

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,148 +1,118 @@
-'use client';
-import { useEffect, useState } from "react";
+"use client";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Header from "./components/header";
+import Footer from "./components/footer";
 import './components/style.css';
 import GoogleIcon from '../../public/images/google.webp';
-import Footer from "./components/footer";
-import { db } from '../../firebase-config';
-import { createUserWithEmailAndPassword, sendEmailVerification, GoogleAuthProvider,onAuthStateChanged,  signInWithPopup, getAuth } from 'firebase/auth';
+import { auth, db } from '../../firebase-config';
+import { RecaptchaVerifier, signInWithPhoneNumber, GoogleAuthProvider, signInWithPopup, PhoneAuthProvider } from 'firebase/auth'; // import PhoneAuthProvider here
 import { doc, setDoc } from 'firebase/firestore';
 
 const Page = () => {
+  const [otp, setOtp] = useState('');
   const [firstname, setFirstname] = useState('');
   const [lastname, setLastname] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [phone, setPhone] = useState('');
+  const [verificationId, setVerificationId] = useState('');
+  const [isOtpSent, setIsOtpSent] = useState(false);
   const [showModal, setShowModal] = useState(false); 
   const [modalMessage, setModalMessage] = useState(''); 
-  const [loadingButton, setLoadingButton] = useState(false)
-  const [isValid, setIsValid] = useState(false)
-  const auth = getAuth();
+  const [loadingButton, setLoadingButton] = useState(false);
   const provider = new GoogleAuthProvider();
-  
   const router = useRouter();
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      setLoadingButton(true);
-  
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-      const user = userCredential.user;
-  
-      await sendEmailVerification(user);
-      setModalMessage('😊 A confirmation link has been sent to your email. Please check your email.');
-      setShowModal(true);
-  
-      const unsubscribe = onAuthStateChanged(auth, async (user) => {
-        if (user && user.emailVerified) {
-          await setDoc(doc(db, 'users', user.uid), {
-            firstname,
-            lastname,
-            email: user.email,
-            uid: user.uid,
-          });
-  
-          const idToken = await user.getIdToken();
-          localStorage.setItem('access_token', idToken);
-  
-          console.log('User successfully created:', user);
-          unsubscribe(); 
-  
-          router.push('/login'); 
-        } else {
-          setModalMessage('Please verify your email before proceeding.');
-          setShowModal(true);
-        }
-      });
-    } catch (error) {
-      console.error('Error signing up:', error.message);
-  
-      if (error.code === 'auth/email-already-in-use') {
-        setModalMessage('This email is already in use. Please use a different one.');
-      } else {
-        setModalMessage('An error occurred during registration.');
-      }
-      setShowModal(true);
-    } finally {
-      setLoadingButton(false);
-    }
-  };
-  
-  
-  
-  const checkVerificationAndCreateUser = async () => {
-    const unsubscribe = auth.onAuthStateChanged(async (user) => {
-      if (user && user.emailVerified) {
-        try {
-          await setDoc(doc(db, 'users', user.uid), {
-            firstname,
-            lastname,
-            email: user.email,
-            uid: user.uid,
-          });
-          console.log('User data created in Firestore:', user);
-          unsubscribe();
-        } catch (error) {
-          console.error('Error creating user data:', error.message);
-        }
-      }
-    });
-  };
-  
-  useEffect(() => {
-    checkVerificationAndCreateUser();
-  }, []);
 
-  const handleGoogleSignUp = async () => {
-    const token = localStorage.getItem('access_token')
-    if(!token){
-      try {
-        const result = await signInWithPopup(auth, provider);
-        const user = result.user;
-    
-        await setDoc(doc(db, 'users', user.uid), {
-          firstname: user.firstname || user.email,
-          lastname: user.lastname || user.email,
-          email: user.email,
-          uid: user.uid,
-        });
-    
-        setModalMessage('🎉 Congratulations! Your account has been created successfully with Google. Happy shopping at Floxsy!');
+  const sendOtp = async () => {
+    try {
+      if (!phone) {
+        setModalMessage('Please enter a valid phone number.');
         setShowModal(true);
-        localStorage.setItem('access_token', await user.getIdToken());
-        console.log('User signed up with Google:', user);
-      } catch (error) {
-        console.error('Error signing up with Google:', error.message);
-        
-        setModalMessage(`An error occurred while signing up with Google: ${error.message}`);
-        setShowModal(true);
+        return;
       }
-    } else {
-      setModalMessage('😃 Great job! You’ve successfully created your account. You can now start using our services')
-      setShowModal(true)
+  
+      window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
+        size: 'invisible', 
+        callback: (response) => {
+          console.log("Recaptcha verified", response);
+        },
+        'expired-callback': () => {
+          console.log("Recaptcha expired");
+        },
+        appVerificationDisabledForTesting: true,
+      }, auth);
+  
+      const appVerifier = window.recaptchaVerifier;
+  
+      const confirmationResult = await signInWithPhoneNumber(auth, `+${phone}`, appVerifier);
+      setVerificationId(confirmationResult.verificationId);
+      setIsOtpSent(true);
+      setModalMessage('OTP has been sent to your phone.');
+      setShowModal(true);
+    } catch (error) {
+      console.error('Error sending OTP:', error);
+      setModalMessage('Failed to send OTP. Please try again.');
+      setShowModal(true);
     }
   };
   
+  
+  
+  const verifyOtp = async () => {
+    try {
+      if (!otp) {
+        setModalMessage('Please enter the OTP.');
+        setShowModal(true);
+        return;
+      }
+  
+      const credential = PhoneAuthProvider.credential(verificationId, otp);
+      const result = await auth.signInWithCredential(credential);
+      const user = result.user;
+  
+      await setDoc(doc(db, 'users', user.uid), {
+        firstname,
+        lastname,
+        phone: user.phoneNumber,
+        uid: user.uid,
+      });
+  
+      setModalMessage('🎉 Account successfully created.');
+      setShowModal(true);
+      router.push('/login');
+    } catch (error) {
+      console.error('Error verifying OTP:', error);
+      setModalMessage('Invalid OTP. Please try again.');
+      setShowModal(true);
+    }
+  };
+  
+  const handleGoogleSignUp = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      await setDoc(doc(db, 'users', user.uid), {
+        firstname: firstname || user.displayName || '',
+        lastname: lastname || '',
+        phone: user.phoneNumber || '',
+        email: user.email,
+        uid: user.uid,
+      });
+
+      setModalMessage('🎉 Signed up successfully with Google!');
+      setShowModal(true);
+      router.push('/login');
+    } catch (error) {
+      console.error('Error with Google sign-up:', error);
+      setModalMessage('Failed to sign up with Google. Please try again.');
+      setShowModal(true);
+    }
+  };
 
   const closeModal = () => {
-    if (modalMessage.startsWith('🎉 Congratulations!')) {
-      router.push('/login');
-    } else {
-      setShowModal(false);
-    }
+    setShowModal(false);
   };
-  
-  const handleEmailChange = (e) => {
-    const value = e.target.value
-    setEmail(value)
-    if(value.endsWith('@gmail.com')){
-      setIsValid(false)
-    } else {
-      setIsValid(true)
-    }
-  }
 
   return (
     <div>
@@ -151,19 +121,24 @@ const Page = () => {
       <div className="container flex flex-col items-center justify-center mt-4">
         <h1 className="text-[#112620] text-center text-[20px] sm:text-[25px] md:text-[30px] xl:text-[35px] font-bold">Create Your Account & Start Shopping!</h1>
         <p className="text-[#112620] text-[13px] sm:text-[16px] md:text-[18px] text-center">Sign up now and begin your shopping journey!</p>
-        <form onSubmit={handleSubmit} className="w-full mt-3 flex flex-col items-center justify-center gap-2 lg:gap-3">
+        <form className="w-full mt-3 flex flex-col items-center justify-center gap-2 lg:gap-3">
           <input className="outline-[#2B4257] p-2 md:p-3 xl:p-4 w-[90%] sm:w-[400px] md:w-[450px] h-[35px] sm:h-[40px] md:h-[45px] text-[12px] sm:text-[15px] lg:text-[17px] border-[1px] border-[#091235] rounded-md" type="text" value={firstname} onChange={(e) => setFirstname(e.target.value)} required placeholder="Enter your Name" />
           <input className="outline-[#2B4257] p-2 md:p-3 xl:p-4 w-[90%] sm:w-[400px] md:w-[450px] h-[35px] sm:h-[40px] md:h-[45px] text-[12px] sm:text-[15px] lg:text-[17px] border-[1px] border-[#091235] rounded-md" type="text" value={lastname} onChange={(e) => setLastname(e.target.value)} required placeholder="Enter your Surname" />
-          <input className="outline-[#2B4257] p-2 md:p-3 xl:p-4 w-[90%] sm:w-[400px] md:w-[450px] h-[35px] sm:h-[40px] md:h-[45px] text-[12px] sm:text-[15px] lg:text-[17px] border-[1px] border-[#091235] rounded-md" type="email" value={email} onChange={handleEmailChange} required placeholder="Enter your Email" />
-          <input className="outline-[#2B4257] p-2 md:p-3 xl:p-4 w-[90%] sm:w-[400px] md:w-[450px] h-[35px] sm:h-[40px] md:h-[45px] text-[12px] sm:text-[15px] lg:text-[17px] border-[1px] border-[#091235] rounded-md" type="password" value={password} onChange={(e) => setPassword(e.target.value)} required placeholder="Enter your Password" />
-          <button disabled={loadingButton || isValid} type="submit" className="bg-[#091235] text-white w-[90%] sm:w-[400px] md:w-[450px] h-[35px] sm:h-[40px] md:h-[45px] text-[12px] sm:text-[15px] lg:text-[17px] rounded-md hover:bg-[#112620]">{loadingButton ? "Creating..." : "Create Account"}</button>
+          <input className="outline-[#2B4257] p-2 md:p-3 xl:p-4 w-[90%] sm:w-[400px] md:w-[450px] h-[35px] sm:h-[40px] md:h-[45px] text-[12px] sm:text-[15px] lg:text-[17px] border-[1px] border-[#091235] rounded-md" type="text" value={phone} onChange={(e) => setPhone(e.target.value)} required placeholder="Enter your Phone Number" />
+          {isOtpSent && (
+            <input className="outline-[#2B4257] p-2 md:p-3 xl:p-4 w-[90%] sm:w-[400px] md:w-[450px] h-[35px] sm:h-[40px] md:h-[45px] text-[12px] sm:text-[15px] lg:text-[17px] border-[1px] border-[#091235] rounded-md" type="text" value={otp} onChange={(e) => setOtp(e.target.value)} required placeholder="Enter the OTP" />
+          )}
+          {!isOtpSent ? (
+            <button type="button" onClick={sendOtp} className="bg-[#091235] text-white w-[90%] sm:w-[400px] md:w-[450px] h-[35px] sm:h-[40px] md:h-[45px] text-[12px] sm:text-[15px] lg:text-[17px] rounded-md hover:bg-[#112620]">Send OTP</button>
+          ) : (
+            <button type="button" onClick={verifyOtp} className="bg-[#091235] text-white w-[90%] sm:w-[400px] md:w-[450px] h-[35px] sm:h-[40px] md:h-[45px] text-[12px] sm:text-[15px] lg:text-[17px] rounded-md hover:bg-[#112620]">Verify OTP</button>
+          )}
         </form>
         <button onClick={handleGoogleSignUp} className="mt-2 w-[90%] sm:w-[400px] md:w-[450px] h-[35px] sm:h-[40px] md:h-[45px] flex items-center justify-center gap-1 text-[12px] sm:text-[15px] lg:text-[17px] border-[1px] border-[#091235] rounded-md bg-transparent">
           <Image className="w-[25px] h-[25px]" src={GoogleIcon} alt="Google icon" />
           Sign Up with Google
         </button>
       </div>
-      {isValid && <p className="text-red-600 text-center mt-2">Email must end with @gmail.com</p>}
       {showModal && (
         <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50 z-50">
           <div className="bg-white p-6 rounded-lg shadow-lg w-[80%] sm:w-96">
@@ -174,6 +149,7 @@ const Page = () => {
         </div>
       )}
 
+      <div id="recaptcha-container"></div>
       <br /><br /><br /><br />
       <Footer />
     </div>
