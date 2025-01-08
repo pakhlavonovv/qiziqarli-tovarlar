a4b803c442b03b36700ed260b867dc07ff09f702
@webdeveloper130390 <abdullokhpakhlavonov@gmail.com>
2025-01-08 11:42:39 +0500

    fixing env

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
 'use client';
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Header from "./components/header";
