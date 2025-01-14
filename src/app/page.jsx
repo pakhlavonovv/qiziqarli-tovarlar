76deb97ceb7898ee4737c8fbaa8f7ef07070d717
@webdeveloper130390 <abdullokhpakhlavonov@gmail.com>
2025-01-15 00:33:13 +0500

    floxsy-change

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,7 @@ const Page = () => {
     <div>
       <Header/>
       <Carousel/>
-      <p className="container mt-1 text-center text-[10px] sm:text-[14px] md:text-[16px]">Floxsy.com Trade Assurance protects you from payment to delivery.</p>
+      <p className="container mt-1 text-center text-[10px] sm:text-[14px] md:text-[16px]">Floxsy.shop Trade Assurance protects you from payment to delivery.</p>
       <div className="mt-[20px] sm:mt-[60px] md:mt-[80px] lg:mt-[100px]">
       <CardsMap/>
       </div>
